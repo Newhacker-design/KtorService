397039a2b908f220b0ef6268d020379e7b06b942
Nguyen Hoang Duong <hoangduchp92@gmail.com>
2026-09-21 22:49:17 +0700

    Fix AIService11

--- src/main/kotlin/com/example/ktorservice/Application.kt
+++ src/main/kotlin/com/example/ktorservice/Application.kt
@@ -5,7 +5,8 @@ import com.example.ktorservice.plugins.configureRouting
 import com.example.ktorservice.plugins.configureSerialization
 import io.ktor.server.application.*
 import io.ktor.server.netty.*
-
+import io.ktor.server.websocket.*
+import kotlin.time.Duration.Companion.seconds
 
 fun Application.module() {
 
@@ -16,6 +17,14 @@ fun Application.module() {
     }
 
     configureSerialization()
+
+    // PHẢI đặt TRƯỚC configureRouting()
+    install(WebSockets) {
+        pingPeriod = 15.seconds
+        timeout = 15.seconds
+        maxFrameSize = Long.MAX_VALUE
+        masking = false
+    }
 
     configureRouting()
 }
